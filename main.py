19b6b9387a8964bb66b9fc5070f9da6b0a93ff6d
Adrian <kuiper2000@hotmail.com>
2024-01-11 18:44:50 +0100

    merge resolve

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 from furhat_remote_api import FurhatRemoteAPI
-<<<<<<< HEAD
 from src.flow import introduction, example, recommendCocktails
-=======
-from src.flow import example, introduction
->>>>>>> main
 
 
 if __name__=='__main__':
     furhat = FurhatRemoteAPI("localhost")
-<<<<<<< HEAD
-    recommendCocktails.run(furhat)
-    
-=======
     introduction.run(furhat)
     
     # Example flow
     # example.run(furhat)
->>>>>>> main
